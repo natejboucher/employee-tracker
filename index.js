f0b712f500e97eb64b7eed12f149d841d358df0f
Nathaniel Boucher <93409883+natejboucher@users.noreply.github.com>
2022-02-25 21:48:36 -0500

    added constructor classes, cleaned up lib, added view roles/dept.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const promptUser = require('./lib/prompt');
+const Prompt = require('./lib/Prompt');
 const db = require('./db/connection');
 
 db.connect(err => {
     if (err) throw err;
     console.log('Database connected.');
-    promptUser();
+    new Prompt().promptUser();
 })
